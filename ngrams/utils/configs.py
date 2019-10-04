b0e65e668dbc6ca700c5ee10cad6bbf63ddc73cc
oya163 <oyeshsin@hotmail.com>
2019-10-04 17:48:59 -0400

    Added configuration for all models

--- ngrams/utils/configs.py
+++ ngrams/utils/configs.py
@@ -17,16 +17,33 @@ class Configuration:
                 with io.open(args.config_file, encoding='utf-8') as f:
                     config = json.load(f)
  
-        self.output_folder = os.path.normpath(config.get("output_folder","./output"))
-        self.data_folder = os.path.normpath(config.get("data_folder","./data"))
-        self.training_file = os.path.normpath(os.path.join(self.data_folder, config.get("training_file","train")))
-        self.dev_file = os.path.normpath(os.path.join(self.data_folder, config.get("dev_file","dev")))
-        self.test_file = os.path.normpath(os.path.join(self.data_folder, config.get("test_file","test")))
+        self.output_folder = args.output_folder
+        self.train_file = args.train_file
+        self.dev_file = args.dev_file
         
         self.model_folder = os.path.normpath(os.path.join(self.output_folder, "models")) 
         self.model_name = args.model_name if args.model_name else config.get("model_name", "default_model")
         self.n_gram = args.n_gram if args.n_gram else config.get("n_gram", 1)
-        self.lambda_val = int(config.get("lambda", 1))
+        self.lambda_val = int(config.get("lambda_val", 1))
+        self.unk_threshold = int(config.get("unk_threshold", 1))
+        self.unk_val = int(config.get("unk_val", 0))
+        self.discount = int(config.get("discount", 0))
+        self.norm_const = int(config.get("norm_const", 0))
+    
+
+    def print_stat(self):
+        print("*********Hyperparameter stat********")
+        print("Output folder: 	", self.output_folder)
+        print("Train file: 	", self.train_file)
+        print("Dev file: 	", self.dev_file)
+        print("Model folder: 	", self.model_folder)
+        print("Model name: 	", self.model_name)
+        print("n-gram: 		", self.n_gram)
+        print("Lambda value: 	", self.lambda_val)
+        print("UNK threshold: 	", self.unk_threshold)
+        print("UNK initial value: ", self.unk_val)
+        print("Discount for backoff: ", self.discount)
+        print("Norm constant: ", self.norm_const)
         
 
 
